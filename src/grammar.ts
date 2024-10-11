5887e05be20abdb9836cd137141071a115683a69
Slava Akhmechet <coffeemug@gmail.com>
2024-10-11 15:13:35 -0700

    got the parser actually working

--- src/grammar.ts
+++ src/grammar.ts
@@ -1,26 +1,27 @@
 import type { parser } from './parser/base';
+import { int, type form, type symbol_ } from './ast';
 import { fwd } from './parser/base';
 import { seq, alpha, many, alnum, nat, either, sepBy } from './parser/lib';
 import { stream } from './parser/stream';
 
 export const expr = fwd(() =>
-  either(form, symbol, nat))
-
-type form = {
-  head: string,
-  parts: expr[],
-}
-
-type expr = number | string | form;
+  either(form, symbol, int))
 
 const form = (source: stream) => {
-  const res: parser<form> = seq(symbol, '[', sepBy(expr, ','), ']').map2((head, _, parts) =>
-    ({ head, parts }));
+  const res: parser<form> = seq(symbol, '[', sepBy(expr, ','), ']').map2<form>((head, _, parts) =>
+    ({ type: 'form', head, parts }));
   return res(source);
 };
 
-const symbol = seq(alpha, many(alnum)).map2((ft, rt) =>
-  [ft, ...rt].join(""));
+const symbol = seq(alpha, many(alnum)).map2<symbol_>((ft, rt) => ({
+  type: 'symbol',
+  value: [ft, ...rt].join(""),
+}));
+
+const int = nat.map<int>(value => ({
+  type: 'int',
+  value,
+}));
 
 /*
   TODO:
