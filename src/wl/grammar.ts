7b7333cf59b889a466139bbc403665dccc673007
Slava Akhmechet <coffeemug@gmail.com>
2024-10-12 19:25:57 -0700

    fixing grammar

--- src/wl/grammar.ts
+++ src/wl/grammar.ts
@@ -4,23 +4,26 @@ import { fwd, lex } from '../parser/base';
 import { seq, alpha, many, alnum, int, either, sepBy, binop, binopr } from '../parser/lib';
 import { stream } from '../parser/stream';
 
-export const expr = fwd(() => either(assign, paren_expr));
+export const expr = fwd(() => assignment);
 
-const paren_expr = (source: stream) => {
-  const p: parser<Expr> = seq('(', expr, ')').map2((_, e) => e);
-  return p(source);
-}
-
-const assign = fwd(() => binopr(either('=', ':='), term, (op, l, r: Expr): Form =>
+const assignment = fwd(() => binopr(either('=', ':='), term, (op, l, r: Expr): Form =>
   new Form(new Symbol(op == '=' ? 'Set' : 'SetDelayed'), [l, r])));
 
 const term = fwd(() => binop(either('+', '-'), factor, (op, l: Expr, r): Form =>
   new Form(new Symbol(op == '+' ? 'Plus' : 'Minus'), [l, r])));
 
-const factor = fwd(() => binop(either('*', '/'), primitive, (op, l: Expr, r): Form =>
+const factor = fwd(() => binop(either('*', '/'), exponent, (op, l: Expr, r): Form =>
   new Form(new Symbol(op == '*' ? 'Times' : 'Div'), [l, r])));
 
-const primitive = fwd(() => either(symbol, integer, form));
+const exponent = fwd(() => binop(either('^'), primitive, (_, l: Expr, r): Form =>
+  new Form(new Symbol('Power'), [l, r])));
+
+const primitive = fwd(() => either(paren_expr, symbol, integer, form));
+
+const paren_expr = (source: stream) => {
+  const p: parser<Expr> = seq('(', expr, ')').map2((_, e) => e);
+  return p(source);
+}
 
 const form = (source: stream) => {
   const res: parser<Form> = seq(symbol, '[', sepBy(expr, ','), ']').map2<Form>((head, _, parts) =>
