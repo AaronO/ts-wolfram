e73bb163b1f0184c684e1d14dd5766a1c22bea42
Slava Akhmechet <coffeemug@gmail.com>
2024-10-14 17:10:49 -0700

    attr vectors, ugly but works

--- src/attrs.ts
+++ src/attrs.ts
@@ -2,16 +2,47 @@ import type { Symbol } from './ast';
 import { symbol } from './symbols';
 import uniq from 'lodash/uniq';
 
-const attrsTable: Map<Symbol, Symbol[]> = new Map();
+type AttrVec = {
+  holdFirst: boolean,
+  holdRest: boolean,
+  protected: boolean,
+  flat: boolean,
+}
+const attrsTable: Map<Symbol, AttrVec> = new Map();
 
 const supported = ['HoldFirst', 'HoldRest', 'HoldAll', 'Protected', 'Flat'];
 
-export const attrs = (sym: Symbol) => attrsTable.get(sym) || [];
+export const attrs = (sym: Symbol) => {
+  const attrVec = getVec(sym);
+  const attrs_: Symbol[] = [];
+  if (attrVec.holdFirst && attrVec.holdRest) {
+    attrs_.push(symbol('HoldAll'));
+  } else {
+    if (attrVec.holdFirst) { attrs_.push(symbol('HoldFirst')); }
+    if (attrVec.holdRest) { attrs_.push(symbol('HoldRest')); }
+  }
+  if (attrVec.protected) { attrs_.push(symbol('Protected')); }
+  if (attrVec.flat) { attrs_.push(symbol('Flat')); }
+  return attrs_;
+}
+
 export const setAttrs = (sym: Symbol, attrs_: Symbol[]) => {
   for(const attr of attrs_) {
     validate(attr);
   }
-  attrsTable.set(sym, uniq([...attrs(sym), ...attrs_]));
+
+  const attrVec = getVec(sym);
+  for (const attr of attrs_) {
+    if (attr.val == 'HoldFirst') { attrVec.holdFirst = true; }
+    if (attr.val == 'HoldRest') { attrVec.holdRest = true; }
+    if (attr.val == 'HoldAll') {
+      attrVec.holdFirst = true;
+      attrVec.holdRest = true;
+    }
+    if (attr.val == 'Protected') { attrVec.protected = true; }
+    if (attr.val == 'Flat') { attrVec.flat = true; }
+  }
+  attrsTable.set(sym, attrVec);
 }
 
 const validate = (sym: Symbol) => {
@@ -19,3 +50,10 @@ const validate = (sym: Symbol) => {
     throw `${sym.val} is not a known attribute.`;
   }
 }
+
+const getVec = (sym: Symbol) => attrsTable.get(sym) || {
+  holdFirst: false,
+  holdRest: false,
+  protected: false,
+  flat: false,
+};
