2aa821498a385553416951e0a9e3a81a97fa45b4
Slava Akhmechet <coffeemug@gmail.com>
2024-10-15 18:42:58 -0700

    blanks

--- src/rewrite.ts
+++ src/rewrite.ts
@@ -1,29 +1,31 @@
 import { Form, Expr, Symbol, Int } from "./ast"
-import { isList } from "./list";
 
 export type Env = Map<Symbol, Expr>;
 
 export const match = (e: Expr, p: Expr): [boolean, Env] => {
   const env: Env = new Map();
 
+  // Treat `HoldPattern` specially
   if (p instanceof Form && p.head instanceof Symbol && p.head.val == 'HoldPattern') {
     p = p.parts[0];
     // TODO: add HoldPattern to builtins, check arg count, set attrs
   }
 
+  // match patterns first
+  if (isBlank(p)) {
+    return [true, env];
+  }
+
+  // The rest is essentially deepEqual
   if (e instanceof Int && p instanceof Int) {
     return [p.val == e.val, env];
-  } else if (e instanceof Symbol && p instanceof Symbol) {
+  }
+  
+  if (e instanceof Symbol && p instanceof Symbol) {
     return [p == e, env];
-  } else if (isList(e) && isList(p)) {
-    if (e.parts.length != p.parts.length) { return [false, env]; }
-    for(let i = 0; i<e.parts.length; i++) {
-      const [matchesp, env_] = match(e.parts[i], p.parts[i]);
-      if (!matchesp) { return [false, env]; }
-      // TODO: merge envs
-    }
-    return [true, env];
-  } else if (e instanceof Form && p instanceof Form) {
+  }
+  
+  if (e instanceof Form && p instanceof Form) {
     if (e.parts.length != p.parts.length) { return [false, env]; }
     
     const [matches, _env] = match(e.head, p.head);
@@ -39,3 +41,6 @@ export const match = (e: Expr, p: Expr): [boolean, Env] => {
 
   return [false, env];
 }
+
+const isBlank = (e: Expr) =>
+  e instanceof Form && e.head instanceof Symbol && e.head.val == 'Blank';
