c6f57724eb559facd63fa3f214e7c7d4e21a880d
Slava Akhmechet <coffeemug@gmail.com>
2024-10-17 21:23:03 -0700

    Loading prelude on startup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { expr } from './grammar';
 import { fromString } from '@spakhm/ts-parsec';
 import { populateBuiltins } from './builtins';
 import { symbol } from './symbols';
+import { promises as fs } from 'fs';
+import * as path from 'path';
 
 function q(rl: Interface, query: string): Promise<string> {
   return new Promise((resolve) => {
@@ -12,6 +14,12 @@ function q(rl: Interface, query: string): Promise<string> {
 
 const main = async () => {
   populateBuiltins();
+  const prelude = expr(fromString(await fs.readFile(path.resolve(__dirname, 'prelude.wl'), 'utf8')));
+  if (prelude.type == 'err') {
+    throw "Couldn't load prelude";
+  } else {
+    prelude.res.eval(new Map());
+  }
 
   const rl = createInterface({
     input: process.stdin,
---
+++ src/prelude.wl
@@ -0,0 +1,10 @@
+D[_?NumberQ, x_Symbol] = 0;
+D[x_, x_Symbol] = 1;
+D[Times[expr1_, expr2_], x_Symbol] =
+  D[expr1, x] expr2 + D[expr2, x] expr1;
+D[Plus[expr1_, expr2_], x_Symbol] = D[expr1, x] + D[expr2, x];
+D[Sin[x_], x_Symbol] = Cos[x];
+D[Cos[x_], x_Symbol] = -Sin[x];
+D[f_Symbol[expr_], x_Symbol] :=
+  (D[f[x], x] /. x -> expr) * D[expr, x];
+D[Power[expr_, p_Integer], x_Symbol] := p expr^(p - 1) * D[expr, x];
